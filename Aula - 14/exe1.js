a590ca1e31a8e8e3a51f0b4b90a6384eddd3e711
DevDiasTha <thaisdiastech@gmail.com>
2023-08-27 19:11:10 -0300

    Laços de repetição, while e do while

---
+++ Aula - 14/exe1.js
@@ -0,0 +1,22 @@
+var contador = 1
+var mult = 0
+
+/* console.log('Tabuada do 2') //faz o teste no inicio do loop
+while(contador <= 10 ){
+
+    mult = contador * 2
+    
+    console.log(`2 X ${contador} = ${mult}`)
+    contador ++
+} */
+
+console.log('Tabuada do 3')
+
+do {   // faz o teste lógico no final do loop
+    mult = contador * 3
+    console.log(`3 X ${contador} = ${mult}`)
+    
+    contador ++
+
+}while(contador <= 10)
+
